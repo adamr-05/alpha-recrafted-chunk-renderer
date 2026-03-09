f4136568945251d60186280f1f592fc9047912a0
Adam <radam05@icloud.com>
2026-03-09 00:40:40 -0400

    add output to image

--- chunk-renderer.py
+++ chunk-renderer.py
@@ -1,5 +1,7 @@
 import gzip
 from block_ids import BLOCK_NAMES
+from block_color import BLOCK_COLORS
+from PIL import Image
 
 with gzip.open('./test_chunk_data/c.0.0.dat','rb') as f:
     data = f.read()
@@ -9,6 +11,7 @@ print(len(data))
 index = data.find(b'Blocks')
 blocksStart = index + 10
 blocks = data[blocksStart:blocksStart + 65536]
+img = Image.new('RGB', (16,16))
 
 for x in range(16):
     row = ""
@@ -17,9 +20,11 @@ for x in range(16):
             index = y + (z*256) + (x*256*16)
             if (blocks[index] != 0):
                 row += f"{blocks[index]:3d}  "
+                img.putpixel((x, z), BLOCK_COLORS[blocks[index]])
                 break
     print(row)
                
+img.save('output.png')
 
 
 
