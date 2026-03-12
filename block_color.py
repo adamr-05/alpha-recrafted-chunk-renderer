7267798d0716c7ea7bcff1c56b3c477cc5026881
Adam <radam05@icloud.com>
2026-03-11 23:09:18 -0400

    finish rgb pixel mapping per block

--- block_color.py
+++ block_color.py
@@ -1,49 +1,70 @@
 BLOCK_COLORS = {
-    1: (128, 128, 128),
+    1: (128, 128, 128), #air
     2: (110, 172, 68), #grass
-    3: (150, 108, 75),
+    3: (150, 108, 75), #dirt
     4: (126, 126, 126), #cobblestone
-    5: (172, 142, 82), #plank
+    5: (172, 142, 82), #planks
     6: (110, 172, 68), #sapling is grass
-    7: (34, 34, 34),
+    7: (34, 34, 34), #bedrock
 
-    8: (35, 50, 195),
-    9: (35, 50, 195),
-    10: (209, 7, 0),
-    11: (209, 7, 0),
-    12: (233, 221, 178),
-    13: (175, 173, 172),
-    14: (177, 145, 44),
-    15: (193, 181, 143),
+    8: (35, 50, 195), #movingwater
+    9: (35, 50, 195), #stillwater
+    10: (209, 7, 0), #movinglava
+    11: (209, 7, 0), #stilllava
+    12: (233, 221, 178), #sand
+    13: (175, 173, 172), #gravel
+    14: (177, 145, 44), #goldOre
+    15: (193, 181, 143), #ironOre
 
-    16: (48, 48, 48),
-    17: (74, 46, 9),
-    18: (7, 77, 4),
-    19: (202, 203, 76),
-    20: (157,187,192),
-    21: (20, 20, 20),
-    22: (250, 5, 0),
-    23: (250, 250, 250),
+    16: (48, 48, 48), #coalOre
+    17: (74, 46, 9), #woodLog
+    18: (7, 77, 4), #leaves
+    19: (202, 203, 76), #sponge
+    20: (157,187,192), #glass
+    21: (20, 20, 20), #coalBlock
+    22: (250, 5, 0), #redstoneBlock
+    23: (250, 250, 250), #cobweb
 
     24: (110, 172, 68), #blue plant is grass
-    26: (255, 255, 90),
+    25: (61, 61, 50), #unlit lamp
+    26: (255, 255, 90), #lit lamp
+    27: (242, 242, 242), #iron pressure plate
+    28: (45, 4, 64), #crying obsidian
+    29: (53, 61, 212), #coral block
     30: (233, 221, 178), #dead bush is sand
+    #31 colored bricks ?????? ----------------------------------
 
-    35: (244, 244, 244),
+    32: (172, 142, 82), #hanging sign (wood texture)
+    35: (244, 244, 244), #wool??? ------------------------------
     37: (110, 172, 68), #plantYellow is grass
     38: (110, 172, 68), #plantRed is grass
     39: (110, 172, 68), #redMushroom grass
 
     40: (110, 172, 68), #brownMushroom grass
+    41: (251, 248, 71), #goldBlock
+    42: (237, 237, 237), #ironBlock
     43: (155, 155, 155), #halfSlab (SMOOTHSLAB texture)
     44: (155, 155, 155), #fullSlab (SMOOTHSLAB texture)
+    45: (125, 58, 50), #brick
+    46: (201, 67, 29), #TNT
+    47: (172, 142, 82), #bookshelf
 
+    48: (141, 165, 141), #mossy cobblestone
+    49: (27, 25, 36), #obsidian
     50: (255, 255, 0), #torch is pure yellow
+    51: (255, 149, 31), #fire
+    52: (31, 58, 79), #mobspawner
     53: (172, 142, 82), #stairs (plank texture)
     54: (168, 116, 43), #chest
     55: (110, 172, 68), #redstone dust (grass texture)
 
+    56: (161, 213, 222), #diamondOre
+    57: (151, 243, 238), #diamondBlock
     58: (98, 54, 19), #crafting table
+    59: (119, 136, 34), #crops
+    60: (106, 69, 40), #tilledField
+    61: (129, 129, 129), #furnaceIdle
+    62: (129, 129, 129), #furnaceActive
     63: (132, 95, 42), #standing sign (plank/wood texture)
 
     64: (132, 95, 42), #wood door
@@ -52,8 +73,15 @@ BLOCK_COLORS = {
     67: (126, 126, 126), #cobblestone stairs
     68: (132, 95, 42), #wall sign (plank/wood texture)
     69: (148, 134, 121), #lever (idk)
+    70: (129, 129, 129), #stone pressurePlate
     71: (193, 193, 193), #iron door
 
+    72: (132, 95, 42), #wood pressurePlate
+    73: (161, 107, 107), #redstoneOre
+    74: (161, 107, 107), #redstoneOre glowing
+    75: (0, 0, 0), #redstone torch idle
+    76: (255, 0, 0), #redstone torch active
+    77: (100, 100, 100), #button
     78: (255, 255, 255), #snow layer
     79: (126, 171, 255), #ice
 
