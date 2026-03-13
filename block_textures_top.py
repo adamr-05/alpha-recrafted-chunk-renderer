3b2d5162086ad7afbeac1d02adb1b38850994dd2
Adam <radam05@icloud.com>
2026-03-13 00:14:03 -0400

    add block_textures_top (texture indexing per block id based on terrain.png)

---
+++ block_textures_top.py
@@ -0,0 +1,137 @@
+BLOCK_TEXTURES_TOP = {
+
+    1: 1, #stone
+    2: 0, #grass
+    3: 2, #dirt
+    4: 16, #cobblestone
+    5: 4, #planks
+    6: 15, #sapling     /t
+    7: 17, #bedrock
+
+    #Textures animated in-game
+    8: 205, #waterMoving
+    9: 205, #waterStill
+    10: 237, #lavaMoving
+    11: 237, #lavaStill
+
+    12: 18, #sand
+    13: 19, #gravel
+    14: 32, #goldOre
+    15: 33, #ironOre
+    16: 34, #coalOre
+    17: 21, #log
+    18: 52, #leaves     /t
+    19: 48, #sponge
+    20: 49, #glass      /t
+    21: 30, #coalBlock
+    22: 46, #redstoneBlock
+    23: 11, #cobweb     /t
+    24: 14, #blueplant  /t
+    25: 62, #unlitLamp
+    26: 78, #litLamp
+    27: 22, #ironPressurePlate  /t ????
+    28: 64, #cryingobsidian
+    29: 76, #coral
+    30: 77, #deadBush
+
+    #need metadata for colors
+    31: 175, #coloredBricks
+    35: 191, #cloth
+
+    #SKIP
+    32: 4, #hangingSign
+
+    37: 13, #yellowPlant    /t
+    38: 12, #redPlant       /t
+    39: 29, #brownMushroom  /t
+    40: 28, #redMushroom    /t
+    41: 23, #goldBlock
+    42: 22, #ironBlock
+    43: 6, #doubleSlab
+    44: 6, #singleSlab
+    45: 7, #brick
+    46: 9, #TNT
+    47: 4, #bookshelf
+    48: 36, #mossycobblestone
+    49: 37, #obsidian
+
+    #need metadata for direction
+    50: 80, #torch          /t
+
+    #SKIP
+    51: 31, #fire ????
+
+    52: 65, #mobSpawner     /t
+    53: 4, #woodStairs
+    54: 25, #chest
+
+    #need metadata for direction
+    55: 84, #redstoneDust   /t
+
+    56: 50, #diamondOre
+    57: 24, #diamondBlock
+    58: 43, #workbench
+
+    #need metadata for growth stage
+    59: 88, #crops -- default seeds     /t
+
+    #need metadata
+    60: 87, #farmland (wet = 86)
+
+    61: 1, #furnaceOff
+    62: 1, #furnaceOn
+
+    #SKIP
+    63: 4, #standingSign    /t
+    64: 97, #woodenDoor     /t
+    65: 83, #ladder         /t
+
+    #need metadata for direction
+    66: 128, #minecartTrack /t
+
+    67: 16, #cobblestoneStairs
+
+    #SKIP
+    68: 4, #wallSign
+
+    #need metadata for direction
+    69: 96, #lever          /t
+
+    70: 1,  #stonePressurePlate     /t
+
+    #SKIP
+    71: 98, #ironDoor       /t
+
+    72: 4, #woodenPressurePlate     /t
+
+    73: 51, #redstoneOre
+    74: 51, #redstoneOreGlowing
+
+    #need metadata for state (position)
+    75: 115, #redstoneTorchOff
+    76: 99, #redstoneTorchOn
+
+    #SKIP
+    77: 1, #button          /t
+
+    78: 66, #snowLayer
+
+    79: 67, #ice
+    80: 66, #snowBlock
+    81: 69, #cactus
+    82: 72, #clay
+    83: 73, #reeds
+    84: 75, #jukebox
+
+    #SKIP
+    85: 4, #fence        /t
+    
+
+
+
+    
+
+
+
+    
+}
